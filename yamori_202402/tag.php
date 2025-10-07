378eb6521eb72492abf0dd74e5c163a4585c1acf
ozaki-work <n.ozaki@arutega.jp>
2025-10-07 18:16:05 +0900

    削除

--- yamori_202402/tag.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ( in_category( array('letter', 'makehouse') ) ) {
-  get_template_part( 'tag-blog' , false );
-} elseif ( in_category( array('casestudy', 'reform', 'store', 'woodenhouse', 'wooden') ) ) {
-  get_template_part( 'tag-works' , false );
-} else {
-  get_template_part( 'tag-blog' , 'normal');
-}
-?>
