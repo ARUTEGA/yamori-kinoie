efd0f41028c70ae0dfb1aa89c2c41d372bbde43b
yangae <cyangae@gmail.com>
2025-10-19 19:52:26 +0900

    blogのフォントサイズがiPhoneで大きくなってしまうバグ

--- header.php
+++ header.php
@@ -11,7 +11,7 @@ if($slug == "top") {
 <html lang="ja">
 	<head>
 		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
 		<link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
