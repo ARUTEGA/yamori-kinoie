c5b5307aba8fd18f09b6f1ab86359f50e642a5fe
yangae <cyangae@gmail.com>
2025-10-20 21:15:45 +0900

    カテゴリによる投稿の出しわけ修正

--- single.php
+++ single.php
@@ -1,8 +1,6 @@
 <?php
-if ( in_category( array('letter', 'makehouse') ) ) {
+if ( in_category( array('letter', 'makehouse', 'works', 'event', 'news', 'openhouse') ) ) {
   get_template_part( 'single-blog' , false );
-} elseif ( in_category( array('openhouse', 'event', 'news') ) ) {
-  get_template_part( 'single-news' , false );
 } elseif ( in_category( array('casestudy', 'reform', 'store', 'woodenhouse', 'wooden') ) ) {
   get_template_part( 'single-works' , false );
 } else {
